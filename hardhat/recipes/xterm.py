d7ca8d66d81bc8af2d3ffd0e4169b18f0f2e389f
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-01-22 19:29:02 -0600

    xterm use libtinfow

--- hardhat/recipes/xterm.py
+++ hardhat/recipes/xterm.py
@@ -4,7 +4,11 @@ from .base import GnuRecipe
 class XTermRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(XTermRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '66fb2f6c35b342148f549c276b12a3aa' \
+                      '3fb408e27ab6360ddec513e14376150b'
+
         self.name = 'xterm'
         self.version = '327'
         self.depends = ['xorg-libs']
         self.url = 'ftp://invisible-island.net/xterm/xterm-$version.tgz'
+        self.environment['LIBS'] += ' -ltinfow'
