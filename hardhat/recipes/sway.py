2927c5d08d59cb512ea51085ed4292de18bf47b3
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-02-15 20:01:57 -0600

    update sway version to current commit

--- hardhat/recipes/sway.py
+++ hardhat/recipes/sway.py
@@ -5,8 +5,8 @@ from ..version import extension_regex
 class SwayRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(SwayRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '3d4d18afc849db8704d8da9cb69eb121' \
-                      'ea92b27d7fa556acdcc6708f05c84964'
+        self.sha256 = '5adeecdbbddfb43f4e9f897187af1742' \
+                      '03554fa0b2ee66b73218a061470f305e'
         self.name = 'sway'
         self.depends = ['cairo', 'dmenu', 'fonts',
                         'gdk-pixbuf',
@@ -14,11 +14,11 @@ class SwayRecipe(GnuRecipe):
                         'pcre', 'pkgconfig', 'util-linux',
                         'wayland', 'wlc', 'xf86-video-fbdev',
                         'xorg-server']
-        # Plus install a specific driver for better performanc
+        # Plus install a specific driver for better performance
         # see recipes/x11/drivers for a list
         # Also install the synaptics driver in that directory
         # for laptop touchpad support
-        self.version = '0.11'
+        self.version = '76614efb16527420017291cd47de176b11440d38'
         self.version_regex = r'(?P<version>\d+\.\d+(-rc\d+)?)' \
             + extension_regex
         self.version_url = 'https://github.com/SirCmpwn/sway/releases'
@@ -50,4 +50,3 @@ class SwayRecipe(GnuRecipe):
         self.log_dir('install', self.directory, 'setuid sway')
         args = ['chmod', '+s', exe]
         self.run_sudo(args)
-
