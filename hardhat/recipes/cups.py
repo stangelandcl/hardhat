331bfc427b8dc339bbbc95a5654308feb555d303
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-02-18 12:34:13 -0600

    add cups sha256

--- hardhat/recipes/cups.py
+++ hardhat/recipes/cups.py
@@ -4,6 +4,9 @@ from .base import GnuRecipe
 class CupsRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(CupsRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'f589bb7d5d1dc3aa0915d7cf2b808571' \
+                      'ef2e1530cd1a6ebe76ae8f9f4994e4f6'
+
         self.description = r'''The Common Unix Printing System (CUPS) is a
 print spooler and associated utilities. It is based on the "Internet Printing
 Protocol" and provides printing services to most PostScript and raster
