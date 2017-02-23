4048dafa89cf430672ce7ea9aed8b568f12287fb
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-02-23 13:09:12 -0600

    start work on tinyproxy config file

--- hardhat/recipes/tinyproxy.py
+++ hardhat/recipes/tinyproxy.py
@@ -1,3 +1,4 @@
+import os
 from .base import GnuRecipe
 
 
@@ -6,7 +7,7 @@ class TinyProxyRecipe(GnuRecipe):
         super(TinyProxyRecipe, self).__init__(*args, **kwargs)
         self.sha256 = 'a41f4ddf0243fc517469cf444c8400e1' \
                       'd2edc909794acda7839f1d644e8a5000'
-                
+
         self.name = 'tinyproxy'
         self.version = '1.8.4'
         self.url = 'https://github.com/tinyproxy/tinyproxy/releases/' \
@@ -21,11 +22,11 @@ class TinyProxyRecipe(GnuRecipe):
 port=47010
 listen=127.0.0.1
 '''
-        filename = os.path.join(self.prefix_dir, 'etc', 'tinyproxy', 'tinyproxy.conf')
+        filename = os.path.join(self.prefix_dir, 'etc', 'tinyproxy',
+                                'tinyproxy.conf')
         dir = os.path.dirname(filename)
         if not os.path.exists(dir):
             os.makedirs(dir)
 
         with open(filename, 'wt') as f:
             f.write(config)
-        
