51eecb7e659fbfa3a62936116e8db2545f12eddc
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-02-23 08:35:49 -0600

    add extra fonts

--- hardhat/recipes/ghostscript.py
+++ hardhat/recipes/ghostscript.py
@@ -6,12 +6,13 @@ from .base import GnuRecipe
 class Extra:
     pass
 
+
 class GhostScriptRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(GhostScriptRecipe, self).__init__(*args, **kwargs)
         self.sha256 = 'a2dec70ab32ee83a05b8e257dfe3f308' \
                       '2ebe3a3d6c0f4c141b3b50ed4bda1564'
-                
+
         self.name = 'ghostscript'
         self.version = '9.20'
         self.simple_version = self.version.replace('.', '')
@@ -40,13 +41,23 @@ class GhostScriptRecipe(GnuRecipe):
             ['ln', '-v', '-s', 'ghostscript', '%s/include/ps' % self.prefix_dir]
         ]
         self.fonts = Extra()
-        self.fonts.url = 'http://downloads.sourceforge.net/gs-fonts/$name-$version.tar.gz'
+        self.fonts.url = 'http://downloads.sourceforge.net/gs-fonts/' \
+                         '$name-$version.tar.gz'
         self.fonts.version = '8.11'
         self.fonts.name = 'ghostscript-fonts-std'
         self.fonts.sha256 = '0eb6f356119f2e49b2563210852e17f5' \
-                            '7f9dcc5755f350a69a46a0d641a0c401'        
-
+                            '7f9dcc5755f350a69a46a0d641a0c401'
         self.extra_downloads.append(self.fonts)
+
+        self.fonts2 = Extra()
+        self.fonts2.url = 'http://downloads.sourceforge.net/gs-fonts/' \
+                          '$name-$version.tar.gz'
+        self.fonts2.version = '6.0'
+        self.fonts2.name = 'gnu-gs-fonts-other'
+        self.fonts2.sha256 = '8146cccc4699fe9dab841446bdd17039' \
+                             'f4769c903eceb54409188b920754aab3'
+
+        self.extra_downloads.append(self.fonts2)
 
     def install(self):
         super(GhostScriptRecipe, self).install()
@@ -56,7 +67,12 @@ class GhostScriptRecipe(GnuRecipe):
                 '%s/share/ghostscript' % self.prefix_dir, '--no-same-owner']
         self.run_exe(args, self.directory, self.environment)
 
-        args = ['fc-cache', '-v', '%s/share/ghostscript/fonts/' % self.prefix_dir]
+        args = ['tar', '-xvf', self.fonts2.filename, '-C',
+                '%s/share/ghostscript' % self.prefix_dir, '--no-same-owner']
+        self.run_exe(args, self.directory, self.environment)
+
+        args = ['fc-cache', '-v',
+                '%s/share/ghostscript/fonts/' % self.prefix_dir]
         self.run_exe(args, self.directory, self.environment)
 
 #    def patch(self):
@@ -66,7 +82,7 @@ class GhostScriptRecipe(GnuRecipe):
 #        shutil.rmtree(os.path.join(self.directory, 'jpeg'))
 #        shutil.rmtree(os.path.join(self.directory, 'libpng'))
 #        shutil.rmtree(os.path.join(self.directory, 'zlib'))
-        
+
         r'''TODO: (From BLFS)
 Optional
 
