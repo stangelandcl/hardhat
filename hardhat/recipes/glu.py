24a440fd60982ffdd44c006781e78ec3d185dfde
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-04-11 09:30:12 -0500

    work on libreoffice

--- hardhat/recipes/glu.py
+++ hardhat/recipes/glu.py
@@ -4,6 +4,9 @@ from .base import GnuRecipe
 class GluRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(GluRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '1f7ad0d379a722fcbd303aa5650c6d7d' \
+                      '5544fde83196b42a73d1193568a4df12'
+
         self.sha256 = None
         self.name = 'glu'
         self.version = '9.0.0'
---
+++ hardhat/recipes/libreoffice.py
@@ -0,0 +1,90 @@
+from .base import GnuRecipe
+
+
+class LibreOfficeRecipe(GnuRecipe):
+    def __init__(self, *args, **kwargs):
+        super(LibreOfficeRecipe, self).__init__(*args, **kwargs)
+        self.name = 'libreoffice'
+        self.version = '5.3.0.3'
+        self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
+        self.depends = [
+            'apr',
+            'boost',
+            'cups',
+            'curl',
+            'dbus-glib',
+            'libjpeg-turbo',
+            'glu',
+            'graphite2',
+            'gtk3',
+            'harfbuzz',
+            'icu',
+            'lcms',
+            'librsvg',
+            'libxml2',
+            'libxslt',
+            'mesa',
+            'nss',
+            'openldap',
+            'openssl',
+            'poppler',
+            'postgres',
+            'python3',
+            'wget',
+            'which',
+            'unzip',
+            'zip'
+            ]
+        self.url = 'http://download.documentfoundation.org/libreoffice/src/' \
+                   '5.3.0/libreoffice-$version.tar.xz'
+        self.compile_args = ['make', 'build-nocheck', '-j%s' % self.cpu_count]
+        self.install_args = ['make', 'distro-pack-install']
+
+    def configure(self):
+        self.configure_args = [
+            'sed', '-e', '"/gzip -f/d"', '-e', '"s|.1.gz|.1|g"',
+            '-i', 'bin/distro-install-desktop-integration']
+        super(LibreOfficeRecipe, self).configure()
+
+        self.configure_args = [
+            'sed', '-e', '"/distro-install-file-lists/d"', '-i', 'Makefile.in']
+        super(LibreOfficeRecipe, self).configure()
+
+        self.configure_args = [
+            'sed', '-e', '"/distro-install-file-lists/d"', '-i', 'Makefile.in']
+        super(LibreOfficeRecipe, self).configure()
+
+        self.configure_args = ['./autogen.sh',
+                               '--prefix=%s' % self.prefix_dir,
+                               '--sysconfdir=%s/etc' % self.prefix_dir,
+                               '--with-vendor=hardhat',
+                               "--with-lang='fr en-GB'",
+                               '--with-help'
+                               '--with-myspell-dicts',
+                               '--with-alloc=system',
+                               '--without-junit'
+                               '--without-system-dicts',
+                               '--disable-dconf',
+                               '--disable-odk',
+                               '--disable-firebird-sdbc',
+                               '--enable-release-build=yes',
+                               '--enable-python=system',
+                               '--with-system-apr',
+                               '--with-system-boost',
+                               '--with-system-cairo',
+                               '--with-system-curl',
+                               '--with-system-expat',
+                               '--with-system-graphite',
+                               '--with-system-harfbuzz',
+                               '--with-system-icu',
+                               '--with-system-jpeg',
+                               '--with-system-lcms2',
+                               '--with-system-libpng',
+                               '--with-system-libxml',
+                               '--with-system-nss',
+                               '--with-system-odbc',
+                               '--with-system-openldap',
+                               '--with-system-openssl',
+                               '--with-system-poppler',
+                               '--with-system-postgresql',
+                               '--with-system-zlib']
