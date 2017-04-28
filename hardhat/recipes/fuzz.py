e8e6a3e1d7fca09f620184ec3aa3b0e24689a45b
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-04-28 16:09:15 -0500

    add fuzz for Z notation

---
+++ hardhat/recipes/fuzz.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+from .base import GnuRecipe
+from hardhat.util import patch
+
+
+class Extra:
+        pass
+
+
+class FuzzRecipe(GnuRecipe):
+    def __init__(self, *args, **kwargs):
+        super(FuzzRecipe, self).__init__(*args, **kwargs)
+        self.description = 'fuzz type-checker for Z by J.M. Spivey'
+        self.sha256 = '3605480594f35678c7db151914ae2b59' \
+                      'f06db86da1f692ff7212f43a96c5c470'
+        self.name = 'fuzz'
+        self.version = '3.4.1'
+        self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
+        self.url = 'http://spivey.oriel.ox.ac.uk/mike/fuzz/fuzz-$version.tgz'
+
+        self.depends = ['make', 'texlive']
+
+        self.manual = Extra()
+        self.manual.url = 'http://spivey.oriel.ox.ac.uk/mike/fuzz/fuzzman.pdf'
+        self.manual.sha256 = '2ffe63543a57b6759179b661c121ef14' \
+                             '1cd483a10801104d549a51a77d4c3469'
+        self.manual.name = 'fuzz-fuzzman'
+        self.manual.version = self.version
+
+        self.reference = Extra()
+        self.reference.url = 'http://spivey.oriel.ox.ac.uk/mike/fuzz/' \
+                             'refcard.pdf'
+        self.reference.sha256 = '274912fc9935597adcd576ef59f9412d' \
+                                'b876ea5569dbfe4ada78346a485a578d'
+        self.reference.name = 'fuzz-refcard'
+        self.reference.version = self.version
+
+        self.extra_downloads.append(self.manual)
+        self.extra_downloads.append(self.reference)
+
+        self.install_args += [
+            'BINDIR=%s/bin' % self.prefix_dir,
+            'LIBDIR=%s/lib' % self.prefix_dir,
+            'TEXDIR=%s/texlive/texmf-local/tex' % self.prefix_dir,
+            'MFDIR=%s/texlive/texmf-local/fonts/source/local' % self.prefix_dir
+            ]
+
+    def patch(self):
+        self.log_dir('patch', self.directory, 'remove strncpy')
+        text = 'EXTERN char *strncpy();'
+        filename = os.path.join(self.directory, 'src', 'zscan.l')
+        patch(filename, text, '')
+
+    def install(self):
+        super(FuzzRecipe, self).install()
+
+        dst = os.path.join(self.prefix_dir, 'doc')
+        shutil.copy2(self.manual.filename, os.path.join(dst, 'fuzzman.pdf'))
+        shutil.copy2(self.reference.filename,
+                     os.path.join(dst, 'Z-refcard.pdf'))
