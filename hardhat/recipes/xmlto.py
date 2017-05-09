8cb2964ad52018abec534cf33c2f7f297e8cd830
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-09 08:07:24 -0500

    xmlto not on fedorahosted anymore

--- hardhat/recipes/xmlto.py
+++ hardhat/recipes/xmlto.py
@@ -6,8 +6,8 @@ class XmltoRecipe(GnuRecipe):
         super(XmltoRecipe, self).__init__(*args, **kwargs)
         self.sha256 = '1130df3a7957eb9f6f0d29e4aa1c7573' \
                       '2a7dfb6d639be013859b5c7ec5421276'
-
+        
         self.name = 'xmlto'
         self.version = '0.0.28'
-        self.url = 'https://fedorahosted.org/releases/x/m/xmlto/' \
-                   'xmlto-$version.tar.bz2'
+        self.url = 'https://launchpad.net/ubuntu/+archive/primary/' \
+                   '+files/xmlto_$version.orig.tar.bz2'
