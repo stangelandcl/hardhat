2744cc6ecb4e2a79a471ecb087d906785ca60dca
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-09 18:17:55 -0500

    update pcre to 8.40

--- hardhat/recipes/pcre.py
+++ hardhat/recipes/pcre.py
@@ -4,12 +4,11 @@ from .base import GnuRecipe
 class PcreRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(PcreRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '9883e419c336c63b0cb5202b09537c14' \
-                      '0966d585e4d0da66147dc513da13e629'
+        self.sha256 = '1d75ce90ea3f81ee080cdc04e68c9c25' \
+                      'a9fb984861a0618be7bbf676b18eda3e'
         self.name = 'pcre'
-        self.version = '8.38'
-        self.url = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/' \
-                   'pcre-$version.tar.gz'
+        self.version = '8.40'
+        self.url = 'https://ftp.pcre.org/pub/pcre/pcre-$version.tar.gz'
         self.depends = ['bzip2', 'readline', 'zlib']
 
         self.configure_args += ['--enable-jit',
