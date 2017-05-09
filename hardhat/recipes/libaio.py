bdce1a1cd94624f192b08ac02b44d7b8d7c08a4d
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-09 06:42:32 -0500

    libaoi moved

--- hardhat/recipes/libaio.py
+++ hardhat/recipes/libaio.py
@@ -6,10 +6,9 @@ class LibaioRecipe(GnuRecipe):
         super(LibaioRecipe, self).__init__(*args, **kwargs)
         self.sha256 = 'e019028e631725729376250e32b47301' \
                       '2f7cb68e1f7275bfc1bbcdd0f8745f7e'
-
         self.name = 'libaio'
         self.version = '0.3.110'
-        self.url = 'https://fedorahosted.org/releases/l/i/libaio/' \
-                   'libaio-0.3.110.tar.gz'
+        self.url = 'https://launchpad.net/ubuntu/+archive/primary/' \
+                   '+files/libaio_$version.orig.tar.gz'
         self.compile_args += ['prefix=%s' % self.prefix_dir]
         self.install_args += ['prefix=%s' % self.prefix_dir]
