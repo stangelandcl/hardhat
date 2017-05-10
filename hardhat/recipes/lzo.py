c5759da143e2218684771d56ac273fc3ac3e16c0
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-10 09:35:21 -0500

    build lzo with fPIC for cairo

--- hardhat/recipes/lzo.py
+++ hardhat/recipes/lzo.py
@@ -6,8 +6,11 @@ class LzoRecipe(GnuRecipe):
         super(LzoRecipe, self).__init__(*args, **kwargs)
         self.sha256 = 'f294a7ced313063c057c504257f437c8' \
                       '335c41bfeed23531ee4e6a2b87bcb34c'
-                      
+
         self.name = 'lzo'
         self.version = '2.09'
         self.url = 'http://www.oberhumer.com/opensource/lzo/' \
                    'download/lzo-$version.tar.gz'
+        # build with -fPIC because cairo tries to use the
+        # static lzo library to make a dynamic library
+        self.configure_args += ['--with-pic']
