a2f8d78f30fd6ef2e126c994654205264eb14346
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-10 07:27:45 -0500

    update cairo 1.14.8

--- hardhat/recipes/cairo.py
+++ hardhat/recipes/cairo.py
@@ -4,7 +4,9 @@ from .base import GnuRecipe
 class CairoRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(CairoRecipe, self).__init__(*args, **kwargs)
-
+        self.sha256 = 'd1f2d98ae9a4111564f6de4e013d639c' \
+                      'f77155baf2556582295a0f00a9bc5e20'
+        
         self.name = 'cairo'
         self.version = '1.14.8'
         self.depends = ['fontconfig', 'glib', 'gtk-doc', 'libpng', 'mesa',
