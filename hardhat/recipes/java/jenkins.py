6d346e6cbec03736cce42f29d11e05574e8043d2
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-16 10:13:21 -0500

    update jenkins

--- hardhat/recipes/java/jenkins.py
+++ hardhat/recipes/java/jenkins.py
@@ -5,11 +5,11 @@ from .base import JarBase
 class JenkinsRecipe(JarBase):
     def __init__(self, *args, **kwargs):
         super(JenkinsRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'efbb4360de55947189f67895be0960dd' \
-                      'fd75104732802948f082f8a5e93228ca'
-
+        self.sha256 = 'aa7f243a4c84d3d6cfb99a218950b8f7' \
+                      'b926af7aa2570b0e1707279d464472c7'
+                
         self.name = 'jenkins'
-        self.version = '2.32.1'
+        self.version = '2.46.2'
         self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
         self.version_url = 'http://mirrors.jenkins.io/war-stable/'
         self.url = 'http://mirrors.jenkins.io/war-stable/$version/jenkins.war'
