2e496bed1fb44a0442e06769691096bee97cd9b1
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-05-26 08:27:58 -0500

    add extract dir

--- hardhat/recipes/cross/cross_glibc.py
+++ hardhat/recipes/cross/cross_glibc.py
@@ -72,6 +72,52 @@ class CrossGLibCRecipe(CrossGnuRecipe):
         dst = '"%s/bin:%s/texlive/bin:%s/java/bin:/bin:/usr/bin"' % (self.prefix_dir, self.prefix_dir, self.prefix_dir)
         patch(filename, src, dst)
 
+        # Intel can build with 2.6.32. Everything else needs 3.2.0 but the configure is
+        # broken to always check for 3.2.0 which is wrong, so remove it
+        self.log_dir('patch', self.extract_dir, 'remove minimum version check on intel')
+        src = r'''
+{ $as_echo "$as_me:${as_lineno-$LINENO}: checking installed Linux kernel header files" >&5
+$as_echo_n "checking installed Linux kernel header files... " >&6; }
+if ${libc_cv_linux320+:} false; then :
+  $as_echo_n "(cached) " >&6
+else
+  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+#include <linux/version.h>
+#if !defined LINUX_VERSION_CODE || LINUX_VERSION_CODE <  (3 *65536+ 2 *256+ 0) /* 3.2.0 */
+# error kernel headers missing or too old
+#endif
+int
+main ()
+{
+
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"; then :
+  libc_cv_linux320='3.2.0 or later'
+else
+  libc_cv_linux320='missing or too old!'
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+fi
+{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $libc_cv_linux320" >&5
+$as_echo "$libc_cv_linux320" >&6; }
+if test "$libc_cv_linux320" != '3.2.0 or later'; then
+  as_fn_error $? "GNU libc requires kernel header files from
+Linux 3.2.0 or later to be installed before configuring.
+The kernel header files are found usually in /usr/include/asm and
+/usr/include/linux; make sure these directories use files from
+Linux 3.2.0 or later.  This check uses <linux/version.h>, so
+make sure that file was built correctly when installing the kernel header
+files.  To use kernel headers not from /usr/include/linux, use the
+configure option --with-headers." "$LINENO" 5
+fi
+'''
+        filename = os.path.join(self.extract_dir, 'sysdeps/unix/sysv/linux')
+        patch(filename, src, '')
+
 
 
     def post_install(self):
