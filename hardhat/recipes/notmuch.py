fb2a8e01c50d0c97975329a23d085e9b978ae19a
Clayton Stangeland <clayton.stangeland@gmail.com>
2017-09-30 07:28:38 -0500

    add notmuch manpages

--- hardhat/recipes/notmuch.py
+++ hardhat/recipes/notmuch.py
@@ -4,13 +4,16 @@ from .base import GnuRecipe
 class NotMuchRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(NotMuchRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '65d28d1f783d02629039f7d15d9a2bad' \
-                      'a147a7d3809f86fe8d13861b0f6ae60b'
+        self.sha256 = 'b4bf09ec9b7b64180704faa26d66cad5' \
+                      'f911a5a00ef812da34cb02c3f8872831'
 
         self.name = 'notmuch'
-        self.version = '0.25'
+        self.version = '0.25.1'
         self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
         self.version_url = 'https://notmuchmail.org/releases'
-        self.depends = ['autotools', 'gmime', 'talloc', 'xapian']
+        self.depends = ['autotools', 'gmime', 'python3-sphinx',
+                        'talloc', 'xapian']
         self.url = 'https://notmuchmail.org/releases/notmuch-$version.tar.gz'
         self.configure_args += ['--without-ruby']
+        # needed to make sphinx test pass to install manpages
+        self.environment['PYTHON'] = 'python3'
