a3eac2cf9a7f2faa5f32de772b2ab0a2b37178a6
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-01-12 10:27:00 -0600

    force use of pgbouncer's getentropy function

--- hardhat/recipes/pgbouncer.py
+++ hardhat/recipes/pgbouncer.py
@@ -14,4 +14,11 @@ class PgBouncerRecipe(GnuRecipe):
         self.depends = ['libevent', 'openssl']
         self.url = 'https://pgbouncer.github.io/downloads/files/$version/' \
                    'pgbouncer-$version.tar.gz'
+        # glibc 2.26 includes a getentropy function
+        # however on linux before 3.17 it is a stub that
+        # always fails. pgbouncer does not properly check
+        # that getentropy() works. Only that it exists.
+        # so disable the use of glibc's getentropy and instead
+        # force use of the internal version
+        self.configure_args += ['ac_cv_func_getentropy=no']
 #        self.configure_strip_cross_compile()
