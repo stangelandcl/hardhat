087ae5313f60a9d38705b49ef6802c610bd9150b
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-01-12 10:23:17 -0600

    update pgbouncer

--- hardhat/recipes/pgbouncer.py
+++ hardhat/recipes/pgbouncer.py
@@ -4,13 +4,14 @@ from .base import GnuRecipe
 class PgBouncerRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(PgBouncerRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'de36b318fe4a2f20a5f60d1c5ea62c1c' \
-                      'a331f6813d2c484866ecb59265a160ba'
+        self.sha256 = 'fa8bde2a2d2c8c80d53a859f8e48bc67' \
+                      '13cf127e31c77d8f787bbc1d673e8dc8'
+
         self.name = 'pgbouncer'
-        self.version = '1.7.2'
+        self.version = '1.8.1'
         self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
         self.version_url = 'https://pgbouncer.github.io/downloads/'
-        self.depends = ['libevent']
+        self.depends = ['libevent', 'openssl']
         self.url = 'https://pgbouncer.github.io/downloads/files/$version/' \
                    'pgbouncer-$version.tar.gz'
 #        self.configure_strip_cross_compile()
