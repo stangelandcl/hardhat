9119bdb2a3508090dce10a76ec2f3da967b0dc12
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-01-17 13:46:39 -0600

    hash update

--- hardhat/recipes/hdf5.py
+++ hardhat/recipes/hdf5.py
@@ -4,8 +4,8 @@ from .base import GnuRecipe
 class Hdf5Recipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(Hdf5Recipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'a4335849f19fae88c264fd0df046bc32' \
-                      '1a78c536b2548fc508627a790564dc38'
+        self.sha256 = 'a4f2db7e0a078aa324f64e0216a80731' \
+                      '731f73025367fa94d158c9b1d3fbdf6f'
         self.name = 'hdf5'
         self.version = '1.8.20'
         version = '.'.join(self.version.split('.')[:2])
