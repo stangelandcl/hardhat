a31c02263e4a3a31248715382d7670b08c712837
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-05 14:24:04 -0600

    update chardet and requests

--- hardhat/recipes/python/chardet.py
+++ hardhat/recipes/python/chardet.py
@@ -4,8 +4,8 @@ from .base import PipBaseRecipe
 class ChardetRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(ChardetRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'e53e38b3a4afe6d1132de62b7400a4ac' \
+                      '363452dc5dfcf8d88e8e0cce663c68aa'
 
         self.name = 'chardet'
         self.version = '2.3.0'
-        self.sha256 = 'e53e38b3a4afe6d1132de62b7400a4ac' \
-                      '363452dc5dfcf8d88e8e0cce663c68aa'
--- hardhat/recipes/python/requests.py
+++ hardhat/recipes/python/requests.py
@@ -4,5 +4,8 @@ from .base import PipBaseRecipe
 class RequestsRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(RequestsRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '9c443e7324ba5b85070c4a818ade28bf' \
+                      'abedf16ea10206da1132edaa6dda237e'
         self.name = 'requests'
         self.version = '2.18.4'
+        self.pydepends = ['chardet']
