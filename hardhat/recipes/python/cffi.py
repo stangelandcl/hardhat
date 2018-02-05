41971b7da61283ae3009fc9c0aae369547ae756d
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-05 15:11:19 -0600

    update cffi

--- hardhat/recipes/python/cffi.py
+++ hardhat/recipes/python/cffi.py
@@ -4,9 +4,7 @@ from .base import PipBaseRecipe
 class CffiRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(CffiRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'b3b02911eb1f6ada203b0763ba924234' \
-                      '629b51586f72a21faacc638269f4ced5'
-
         self.name = 'cffi'
-        self.version = '1.10.0'
+        self.version = '1.11.4'
+        self.depends = ['libffi']
         self.pydepends = ['pycparser']
