3d8f2d4dc22bd40edfc258fef53fe7b70fede55a
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-05 15:09:06 -0600

    depends

--- hardhat/recipes/python/argon2_cffi.py
+++ hardhat/recipes/python/argon2_cffi.py
@@ -6,7 +6,7 @@ class Argon2CffiRecipe(PipBaseRecipe):
         super(Argon2CffiRecipe, self).__init__(*args, **kwargs)
         self.sha256 = '99a5246d68fb17c33eddc9e8b21cfcb0' \
                       'de1f8360d0bb0b73be5ede20078de3aa'
-                
+
         self.name = 'argon2_cffi'
         self.version = '16.3.0'
-        self.pydepends = ['six']
+        self.pydepends = ['cffi', 'six']
--- hardhat/recipes/python/six.py
+++ hardhat/recipes/python/six.py
@@ -4,8 +4,7 @@ from .base import PipBaseRecipe
 class SixRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(SixRecipe, self).__init__(*args, **kwargs)
-
-        self.name = 'six'
-        self.version = '1.10.0'
         self.sha256 = '105f8d68616f8248e24bf0e9372ef04d' \
                       '3cc10104f1980f54d57b2ce73a5ad56a'
+        self.name = 'six'
+        self.version = '1.10.0'
