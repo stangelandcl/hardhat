2d46217c7e21f56c198a16cae701b183f8837496
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-05 15:43:18 -0600

    hash

--- hardhat/recipes/python/setuptools_scm.py
+++ hardhat/recipes/python/setuptools_scm.py
@@ -4,6 +4,8 @@ from .base import PipBaseRecipe
 class SetupToolsScmRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(SetupToolsScmRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'e163e8a12d2121f77575773cfc2b5988' \
+                      '275dc1f1d2541fdf780127c29dbbea9c'
         self.name = 'setuptools_scm'
         self.version = '1.15.7'
         self.pydepends = ['setuptools']
