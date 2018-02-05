777c9c5266348c709cf3f682edcfaf63f82fe3a7
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-05 14:22:50 -0600

    update requests

--- hardhat/recipes/python/requests.py
+++ hardhat/recipes/python/requests.py
@@ -4,8 +4,5 @@ from .base import PipBaseRecipe
 class RequestsRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(RequestsRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'b3b191d677e526c1e512db86bc7387cc' \
-                      'b8356e8826bcc7faa07f78f09afe68dd'
-
         self.name = 'requests'
-        self.version = '2.14.1'
+        self.version = '2.18.4'
