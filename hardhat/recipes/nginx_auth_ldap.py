9384168ca81425a706522f1c4e1b4ec302b5ba22
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-02-23 10:31:13 -0600

    update nginx auth

--- hardhat/recipes/nginx_auth_ldap.py
+++ hardhat/recipes/nginx_auth_ldap.py
@@ -6,11 +6,10 @@ from .base import GnuRecipe
 class NginxAuthLdapRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(NginxAuthLdapRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'e3cfe3a05c6d0bf34df3f64dcf72a4d2' \
-                      '2e4c408817da7ab2f13989d73a4160f3'
-                
+        self.sha256 = '83938e4ae7bac87440b8ea920987ca2f' \
+                      '5407ea86c37b439b61ddfc3ad1347891'        
         self.name = 'nginx-auth-ldap'
-        self.version = '9f8b59263f6bf36ddfc831554d6b05579794aa9e'
+        self.version = '12fed6b82954d8b85d17c7e360d35c2d1eb3d3f9'
         self.url = self.github_commit('stangelandcl')
 
     def configure(self):
