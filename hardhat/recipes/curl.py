0486573843b703136b9a4c1136ec311f3e069cd8
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-05-15 17:05:30 -0500

    update curl to include mime API

--- hardhat/recipes/curl.py
+++ hardhat/recipes/curl.py
@@ -5,11 +5,11 @@ from .cacert import CACertRecipe
 class CurlRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(CurlRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'de60a4725a3d461c70aa571d7d69c788' \
-                      'f1816d9d1a8a2ef05f864ce8f01279df'
+        self.sha256 = 'b5920ffd6a8c95585fb95070e0ced383' \
+                      '22790cb335c39d0dab852d12e157b5a0'
 
         self.name = 'curl'
-        self.version = '7.56.0'
+        self.version = '7.59.0'
         self.depends = ['cacert', 'openldap', 'openssl', 'zlib']
         self.url = 'https://curl.haxx.se/download/curl-$version.tar.bz2'
         self.configure_args += [
