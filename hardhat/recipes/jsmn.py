8ddc4aa2351c98f53fa4b7538610f3d3fa5082a9
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-05-16 12:23:05 -0500

    update jsmn

--- hardhat/recipes/jsmn.py
+++ hardhat/recipes/jsmn.py
@@ -6,11 +6,11 @@ from ..util import patch
 class JsmnRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(JsmnRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '422be63017eb7afdf1c0be4fcb0f7be7' \
-                      '40447b7e6dbec99f6121491d7a11b3bb'
+        self.sha256 = '1b153d8cc80d409ba2b7d34af9c6573f' \
+                      '6a966b81c4383b1b6e0f111c7ff96741'
 
         self.name = 'jsmn'
-        self.version = '436a26632d657917c2bcc4e24bfb67644d59887a'
+        self.version = 'f370fb45dbff85619c127e40945e1150d5d7b407'
         self.version_regex = r'(?P<version>\d+\.\d+\.\d+)'
         self.depends = []
         self.url = self.github_commit('stangelandcl')
