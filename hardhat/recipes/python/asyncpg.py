15a5ad43a2946d0ca2c6005888fde9c58e630c2d
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-07-11 17:36:57 -0500

    update pytz and asyncpg

--- hardhat/recipes/python/asyncpg.py
+++ hardhat/recipes/python/asyncpg.py
@@ -4,9 +4,9 @@ from .base import PipBaseRecipe
 class AsyncPgRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(AsyncPgRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '31d5a9d993ce97924d9601bf6a37bb8b' \
-                      '542d63bc8716b36238511e5e5915b14c'
+        self.sha256 = '10efb357cf879f0e7e3ea4ea852b0b52' \
+                      '877f208d988aeb626296092d2601e3cd'
 
         self.name = 'asyncpg'
         self.depends = ['postgres']
-        self.version = '0.16.0'
+        self.version = '0.17.0'
--- hardhat/recipes/python/pytz.py
+++ hardhat/recipes/python/pytz.py
@@ -4,7 +4,8 @@ from .base import PipBaseRecipe
 class PytzRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(PytzRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'f5c056e8f62d45ba8215e5cb8f50dfcc' \
-                      'b198b4b9fbea8500674f3443e4689589'
+        self.sha256 = 'ffb9ef1de172603304d9d2819af6f5ec' \
+                      'e76f2e85ec10692a524dd876e72bf277'
+
         self.name = 'pytz'
-        self.version = '2017.2'
+        self.version = '2018.5'
