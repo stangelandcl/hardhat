075377aaed48e50d922db860d48ca4a2ff7c8d8a
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-07-12 08:12:38 -0500

    work on pandas

--- hardhat/recipes/python/pandas.py
+++ hardhat/recipes/python/pandas.py
@@ -4,10 +4,13 @@ from .base import PipBaseRecipe
 class PandasRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(PandasRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '9cd3614b4e31a0889388ff1bd19ae857' \
+                      'ad52658b33f776065793c293a29cf612'
+
         self.name = 'pandas'
         self.version = '0.23.3'
         self.pydepends = ['numpy', 'dateutil', 'scipy', 'xlwt']
 
         self.environment['CFLAGS'] += \
-            ' -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION'
+            ' -DNPY_NO_DEPRECATED_API=NPY_1_6_API_VERSION'
         self.environment['CXXFLAGS'] = self.environment['CFLAGS']
