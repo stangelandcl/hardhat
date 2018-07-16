ee27764a75314ccb3c82d9f14651c97b2ac2c674
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-07-16 15:35:10 -0500

    zstd 1.3.5

--- hardhat/recipes/zstd.py
+++ hardhat/recipes/zstd.py
@@ -5,10 +5,11 @@ from ..version import extension_regex
 class ZstdRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(ZstdRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = '92e41b6e8dd26bbd46248e8aa1d86f15' \
-                      '51bc221a796277ae9362954f26d605a9'
+        self.sha256 = 'd6e1559e4cdb7c4226767d4ddc990bff' \
+                      '5f9aab77085ff0d0490c828b025e2eea'
+
         self.name = 'zstd'
-        self.version = '1.3.4'
+        self.version = '1.3.5'
         self.version_regex = 'v(?P<version>\d+\.\d+\.\d+)' + extension_regex
         self.version_url = 'https://github.com/facebook/zstd/releases'
         self.url = 'https://github.com/facebook/zstd/archive/v$version.tar.gz'
