48aca87de26cad6ba28c97f2c60dc8503f17843a
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-26 06:35:57 -0500

    gawk sha256

--- hardhat/recipes/gawk.py
+++ hardhat/recipes/gawk.py
@@ -5,6 +5,8 @@ from ..urls import Urls
 class GawkRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(GawkRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'd1119785e746d46a8209d28b2de404a5' \
+                      '7f983aa48670f4e225531d3bdc175551'
         self.name = 'gawk'
         self.version = '4.2.1'
         self.url = Urls.gnu_template(self.name, self.version, 'tar.xz')
