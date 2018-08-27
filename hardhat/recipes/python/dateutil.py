0ea7aaf120ef3e2b6f90f3cda4011fd9d663bd7a
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-27 02:34:28 -0500

    dateutil fix

--- hardhat/recipes/python/dateutil.py
+++ hardhat/recipes/python/dateutil.py
@@ -1,13 +1,16 @@
-from .base import PipBaseRecipe
+from .base import SetupPyRecipe
 
 
-class DateUtilRecipe(PipBaseRecipe):
+class DateUtilRecipe(SetupPyRecipe):
     def __init__(self, *args, **kwargs):
         super(DateUtilRecipe, self).__init__(*args, **kwargs)
         self.sha256 = 'e27001de32f627c22380a688bcc43ce8' \
                       '3504a7bc5da472209b4c70f02829f0b8'
 
         self.name = 'dateutil'
-        self.pydepends = ['setuptools', 'six']
+        self.pydepends = ['setuptools_scm', 'six']
         self.version = '2.7.3'
         self.pypi_name = 'python-dateutil'
+        self.url = 'https://files.pythonhosted.org/packages/a0/b0/' \
+                   'a4e3241d2dee665fea11baec21389aec688' \
+                   '6655cd4db7647ddf96c3fad15/python-dateutil-$version.tar.gz'
--- hardhat/recipes/python/setuptools_scm.py
+++ hardhat/recipes/python/setuptools_scm.py
@@ -4,8 +4,9 @@ from .base import PipBaseRecipe
 class SetupToolsScmRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(SetupToolsScmRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'e163e8a12d2121f77575773cfc2b5988' \
-                      '275dc1f1d2541fdf780127c29dbbea9c'
+        self.sha256 = '1191f2a136b5e86f7ca8ab00a97ef7ae' \
+                      'f997131f1f6d4971be69a1ef387d8b40'
+
         self.name = 'setuptools_scm'
-        self.version = '1.15.7'
+        self.version = '3.1.0'
         self.pydepends = ['setuptools']
