577e5dcf8d0833133321b7e4b507aad4f8de9ed9
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-27 07:17:11 -0500

    aiohttp

--- hardhat/recipes/python/aiohttp.py
+++ hardhat/recipes/python/aiohttp.py
@@ -1,11 +1,11 @@
-from .base import PipBaseRecipe
+from .base import SetupPyRecipe
 
 
-class AioHttpRecipe(PipBaseRecipe):
+class AioHttpRecipe(SetupPyRecipe):
     def __init__(self, *args, **kwargs):
         super(AioHttpRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'f20deec7a3fbaec7b5eb7ad99878427a' \
-                      'd2ee4cc16a46732b705e8121cbb3cc12'
+        self.sha256 = '9b15efa7411dcf3b59c1f4766eb16ba1' \
+                      'aba4531a33e54d469ee22106eabce460'
 
         self.pythons = ['python3']
         self.pydepends = ['async_timeout',
@@ -16,4 +16,5 @@ class AioHttpRecipe(PipBaseRecipe):
                           'multidict',
                           'yarl']
         self.name = 'aiohttp'
-        self.version = '3.3.2'
+        self.version = '3.4.0'
+        self.url = 'https://files.pythonhosted.org/packages/09/01/22c9b713b195d071b73fdc2f977f8717497b0d30c41c0b4a9cd908b925ec/aiohttp-3.4.0.tar.gz'
