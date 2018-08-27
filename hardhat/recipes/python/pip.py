3eb54b8b7207f7d74cddabec29bf2203d0a52c20
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-27 07:03:30 -0500

    setup.py

--- hardhat/recipes/python/pip.py
+++ hardhat/recipes/python/pip.py
@@ -1,9 +1,13 @@
-from .base import PipBaseRecipe
+from .base import SetupPyRecipe
 
 
-class PipRecipe(PipBaseRecipe):
+class PipRecipe(SetupPyRecipe):
     def __init__(self, *args, **kwargs):
         super(PipRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'a0e11645ee37c90b40c46d607070c4fd' \
+                      '583e2cd46231b1c06e389c5e814eed76'
 
         self.name = 'pip'
         self.version = '18.0'
+        self.url = 'https://files.pythonhosted.org/packages/69/81/' \
+                   '52b68d0a4de760a2f1979b0931ba7889202f302072cc7a0d614211bc7579/pip-$version.tar.gz'
