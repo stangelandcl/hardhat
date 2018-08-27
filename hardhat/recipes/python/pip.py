6a53e7d58bd8ff7c516834119f3c7a77abf3b484
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-27 07:01:58 -0500

    pip version

--- hardhat/recipes/python/pip.py
+++ hardhat/recipes/python/pip.py
@@ -6,6 +6,4 @@ class PipRecipe(PipBaseRecipe):
         super(PipRecipe, self).__init__(*args, **kwargs)
 
         self.name = 'pip'
-        self.version = '8.1.1'
-        self.sha256 = '3e78d3066aaeb633d185a57afdccf700' \
-                      'aa2e660436b4af618bcb6ff0fa511798'
+        self.version = '18.0'
--- hardhat/recipes/python/setuptools.py
+++ hardhat/recipes/python/setuptools.py
@@ -4,6 +4,8 @@ from .base import PipBaseRecipe
 class SetupToolsRecipe(PipBaseRecipe):
     def __init__(self, *args, **kwargs):
         super(SetupToolsRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '47881d54ede4da9c15273bac65f9340f' \
+                      '8929d4f0213193fa7894be384f2dcfa6'
         self.name = 'setuptools'
         self.version = '40.2.0'
         self.pydepends = ['appdirs']
