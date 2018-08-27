8b4b05acd85d4b5974f5f1aaef07a06a5870bc03
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-26 19:52:23 -0500

    update libpipeline

--- hardhat/recipes/libpipeline.py
+++ hardhat/recipes/libpipeline.py
@@ -5,9 +5,8 @@ from ..urls import Urls
 class LibPipelineRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(LibPipelineRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = 'da46d7b20163aadb9db2faae483f734e' \
-                      '9096a7550c84b94029abeab62dd1b9ee'
-
+        self.sha256 = '0d72e12e4f2afff67fd7b9df0a24d7ba' \
+                      '42b5a7c9211ac5b3dcccc5cd8b286f2b'
         self.name = 'libpipeline'
-        self.version = '1.4.1'
+        self.version = '1.5.0'
         self.url = Urls.savannah(self.name, self.version)
