35cd81c5d8440f17a153c7b7a1d86d92759474fc
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-27 19:12:20 -0500

    fix dmenu

--- hardhat/recipes/dmenu.py
+++ hardhat/recipes/dmenu.py
@@ -4,12 +4,12 @@ from .base import GnuRecipe
 class DMenuRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(DMenuRecipe, self).__init__(*args, **kwargs)
-        self.sha256 = None  # hash changes every time
-
+        self.sha256 = 'fe615a5c3607061e2106700862e82ac6' \
+                      '2a9fa1e6a7ac3d616a9c76106476db61'
         self.name = 'dmenu'
-        self.version = 'f428f3e01a4ced5b1df07ddf913bb022692f8035'
+        self.version = '4.8'
         self.depends = ['freetype', 'xorg-libs']
-        self.url = 'http://git.suckless.org/dmenu/snapshot/dmenu-$version.tar.gz'
+        self.url = 'https://dl.suckless.org/tools/dmenu-$version.tar.gz'
         self.compile_args += [
             'PREFIX=%s' % self.prefix_dir,
             'FREETYPEINC=%s/include/freetype2' % self.prefix_dir]
--- hardhat/recipes/flac.py
+++ hardhat/recipes/flac.py
@@ -4,6 +4,8 @@ from .base import GnuRecipe
 class FlacRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(FlacRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = '91cfc3ed61dc40f47f050a109b086106' \
+                      '67d73477af6ef36dcad31c31a4a8d53f'
         self.name = 'flac'
         self.depends = ['doxygen', 'libogg', 'yasm']
         self.version = '1.3.2'
