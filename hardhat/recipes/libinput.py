b5b2262f8bba2998bed1fadf2f9551e015fe06c8
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-31 09:59:18 -0500

    set hash

--- hardhat/recipes/libinput.py
+++ hardhat/recipes/libinput.py
@@ -4,6 +4,8 @@ from .base import GnuRecipe
 class LibInputRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(LibInputRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'f31191d96e425b4f16319842279d6594' \
+                      '6d9d983dcd3d9e466ae1206aa10ecb06'
         self.name = 'libinput'
         self.version = '1.11.3'
         self.depends = ['check', 'doxygen', 'dot', 'gtk3',
