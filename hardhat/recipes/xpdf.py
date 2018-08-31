f1614d103435d721646983f3401c16ee65b6d9fe
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-31 18:19:14 -0500

    xpdf 4.00

--- hardhat/recipes/xpdf.py
+++ hardhat/recipes/xpdf.py
@@ -4,8 +4,13 @@ from .base import GnuRecipe
 class XpdfRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(XpdfRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'ff3d92c42166e35b1ba6aec9b5f0adff' \
+                      'b5fc05a3eb95dc49505b6e344e4216d6'
+
         self.name = 'xpdf'
-        self.version = '3.02'
-        self.depends = ['lesstiff']
-        self.url = 'http://gd.tuwien.ac.at/publishing/xpdf/' \
-                   'xpdf-$version.tar.gz'
+        self.version = '4.00'
+        self.depends = ['cmake', 'freetype', 'libpng', 'zlib']
+        self.url = 'https://xpdfreader-dl.s3.amazonaws.com/xpdf-$version.tar.gz'
+        self.configure_args = ['cmake', '-G', '"Unix Makefiles"',
+                               '-DCMAKE_BUILD_TYPE=Release',
+                               '-DCMAKE_INSTALL_PREFIX=%s' % self.prefix_dir]
