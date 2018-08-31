913e33dd2705e22dfd3eab921a7bda27d8e02c0d
Clayton Stangeland <clayton.stangeland@gmail.com>
2018-08-31 15:07:07 -0500

    hash lesstiff

--- hardhat/recipes/lesstif.py
+++ hardhat/recipes/lesstif.py
@@ -4,6 +4,8 @@ from .base import GnuRecipe
 class LessTiffRecipe(GnuRecipe):
     def __init__(self, *args, **kwargs):
         super(LessTiffRecipe, self).__init__(*args, **kwargs)
+        self.sha256 = 'e5ef90ff30897448a7c090c2e31ceb30' \
+                      '2ed064a60411436e8995580848ed1a63'
         self.name = 'lesstiff'
         self.version = '0.95.0'
         self.depends = ['x11']
